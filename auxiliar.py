0c7493e9f8e9d3cbb1d40a0eb1cd8ba18e60739c
Rafael Pinto <rafaelzez@gmail.com>
2022-02-25 10:40:13 -0300

    Organização do código

--- auxiliar.py
+++ auxiliar.py
@@ -20,12 +20,11 @@ def inteiro_para_romano(numero):
 
 def inserir_no_mongodb(valor, operacao, browser_info):
     cliente = pymongo.MongoClient('<Connection String do MongoDB>')
-
     database = cliente['<Seu database>']
     collection = database['<A collection específica do database selecionado>']
+    
     dicionario = browser_info
     data_atual = datetime.now().strftime("%d/%m/%Y - %H:%M:%S")
-
     dicionario['data'] = data_atual
     dicionario['valor'] = valor
     dicionario['operacao'] = operacao
